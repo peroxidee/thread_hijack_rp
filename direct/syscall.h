d20f4a40f0a94c1d173a5831e2c12947c6868aa2
0xfffriday <115765212+peroxidee@users.noreply.github.com>
2025-10-08 00:24:09 -0700

    midnight

---
+++ direct/syscall.h
@@ -0,0 +1,72 @@
+#ifndef _SYSCALLS_H
+#define _SYSCALLS_H
+
+
+#include <windows.h>
+
+
+
+#ifdef __cplusplus   
+extern "C" {        
+#endif
+
+typedef long NTSTATUS;
+typedef NTSTATUS* PNTSTATUS
+
+extern NTSTATUS NtAllocateVirtualMemory(
+    HANDLE ProcessHandle,    
+    PVOID* BaseAddress,      
+    ULONG_PTR ZeroBits,      
+    PSIZE_T RegionSize,      
+    ULONG AllocationType,    
+    ULONG Protect            
+);
+
+extern NTSTATUS NtSuspendThread(
+    HANDLE ThreadHandle,     
+    PULONG PreviousSuspendCount 
+);
+
+extern NTSTATUS NtWriteVirtualMemory(
+    HANDLE ProcessHandle,     
+    PVOID BaseAddress,      
+    PVOID Buffer,             
+    SIZE_T NumberOfBytesToWrite, 
+    PULONG NumberOfBytesWritten 
+);
+
+extern NTSTATUS NtOpenThread(
+	PHANDLE ThreadHandle,
+	ACCESS_MASK DesiredAccess,
+	POBJECT_ATTRIBUTES ObjectAttributes,
+	PCLIENT_ID ClientId
+
+);
+
+extern NTSTATUS NtOpenProcess(
+	PHANDLE ThreadHandle,
+	ACCESS_MASK DesiredAccess,
+	POBJECT_ATTRIBUTES ObjectAttributes,
+	PCLIENT_ID ClientId
+);
+
+
+
+extern NTSTATUS NtGetContextThread(
+    HANDLE ThreadHandle,
+    PCONTEXT ThreadContext
+);
+extern NTSTATUS NtSetContextThread(
+    HANDLE ThreadHandle,
+    PCONTEXT ThreadContext
+);
+
+extern NTSTATUS NtResumeThread(
+    HANDLE ThreadHandle,
+    PULONG SuspendCount
+);
+
+#ifdef __cplusplus  
+}
+#endif
+#endif // _SYSCALLS_H
